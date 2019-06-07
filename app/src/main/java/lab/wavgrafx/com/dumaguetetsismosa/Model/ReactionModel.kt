953dce1b49684f931229f5c514cfc01e50cc3422
wavgrafx <49887967+wavgrafx@users.noreply.github.com>
2019-06-08 00:57:39 +0800

    Initial commit

---
+++ app/src/main/java/lab/wavgrafx/com/dumaguetetsismosa/Model/ReactionModel.kt
@@ -0,0 +1,9 @@
+package lab.wavgrafx.com.dumaguetetsismosa.Model
+
+class ReactionModel (
+    val Phone: String = "",
+    val Alias: String = "",
+    val Reaction: String = "",
+    val Blocked: String = "",
+    val DateTimeSent: String = ""
+)
